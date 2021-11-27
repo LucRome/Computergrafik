2d2e9f32a02c99afe26d9c45a8708cad4d43db25
LucRome <64947931+LucRome@users.noreply.github.com>
2021-11-28 00:55:19 +0100

    add useful Coordinates/Vectors as Constants

--- Raytracer/src/raytracer/coordinate_utils.py
+++ Raytracer/src/raytracer/coordinate_utils.py
@@ -15,3 +15,12 @@ def normalise(vec: np.ndarray) -> np.ndarray:
 
 def is_normalised(vec: np.ndarray) -> bool:
     return (np.linalg.norm(vec) == 1)
+
+"""
+Useful Coordinates
+"""
+
+ZERO = np.array([0,0,0], dtype=np.float64)
+CAMERA_SCREEN_MIDDLE = np.array([0,0,-1], dtype=np.float64)
+CAMERA_SCREEN_DIR1 = np.array([1,0,0], dtype=np.float64)
+CAMERA_SCREEN_DIR2 = np.array([0,1,0], dtype=np.float64)
