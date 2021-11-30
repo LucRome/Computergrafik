8550c8ef0c21c399f96ac5646f06b72c2d069c78
LucRome <64947931+LucRome@users.noreply.github.com>
2021-11-30 17:21:26 +0100

    fix camera

--- Raytracer/src/raytracer/camera.py
+++ Raytracer/src/raytracer/camera.py
@@ -1,8 +1,8 @@
 from typing import Iterable
 import numpy as np
-from Raytracer.src.raytracer.coordinate_utils import ZERO
+from raytracer.coordinate_utils import ZERO
 
-from coordinate_utils import Ray
+from raytracer.coordinate_utils import Ray
 
 "TODO: Tests"
 class SimpleCamera():
@@ -14,7 +14,7 @@ class SimpleCamera():
 
     def __init__(self, width: np.int16, height: np.int16, fov: np.float16) -> None:
         [self.width, self.height, self.fov] = [width, height, fov]
-        self.tan_fov_half = np.rad2deg(np.tan(np.divide(fov, 2)))
+        self.tan_fov_half = np.tan(np.deg2rad(np.divide(fov, 2)))
         self.aspect_ratio = np.divide(width, height)
     
     def get_primary_rays(self) -> Iterable[Ray]: #Correct Return Type ?
@@ -22,9 +22,12 @@ class SimpleCamera():
         usage: for ray in camera.get_primary_rays(): ...
         """
         for y in range(0, self.height):
-            pixel_screen_y: np.float64 = np.divide(y, self.height)
-            pixel_cam_y = np.multiply(np.subtract(1, np.multiply(2, pixel_screen_y)), self.tan_fov_half)
+            pixel_screen_y: np.float64 = np.divide(y+0.5, self.height)
+            pixel_cam_y = (1 - 2*pixel_screen_y) * self.tan_fov_half #np.multiply(np.subtract(1, np.multiply(2, pixel_screen_y)), self.tan_fov_half)
             for x in range(0, self.width):
-                pixel_screen_x: np.float64 = np.divide(x, self.width)
-                pixel_cam_x = np.multiply(np.multiply(np.subtract(np.multiply(2, pixel_screen_x), 1), self.aspect_ratio), self.tan_fov_half)
+                pixel_screen_x: np.float64 = np.divide(x+0.5, self.width)
+                pixel_cam_x = (2*pixel_screen_x - 1) * self.aspect_ratio * self.tan_fov_half #np.multiply(np.multiply(np.subtract(np.multiply(2, pixel_screen_x), 1), self.aspect_ratio), self.tan_fov_half)
                 yield(Ray(ZERO, [pixel_cam_x, pixel_cam_y, -1]))
+
+    def set_resolution(self, width: np.int16, height: np.int16) -> None:
+        [self.width, self.height] = [width, height]
